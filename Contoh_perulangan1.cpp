8655e27c0c7b8868221f2b4f034b99ee29df954e
restuchan <restu.chan188@gmail.com>
2025-03-17 15:40:34 +0700

    perulangan while

---
+++ Contoh_perulangan1.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstime>
+using namespace std
+
+int main(){
+    int n;
+    int perulanganWhile = 0;
+    int perulanganDo = 0;
+
+    //Perulangan dengan for 
+
+    cout << "Perulangan for pencacah naik" << endl;
+    for (n = 0; n < 5; n++)
+    {
+        cout << "Nilai n = " << n << " Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhir = " << n << endl;
+    cout << endl;
+
+
+    cout << "Perulangan for pencacah turun" << endl;
+    for (n = 5; n > 0; n--)
+    {
+        cout << "Nilai n = " << n << "Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhir = " << n;
+    cout << endl;
+
+
+    cout << "Perulangan while" << endl;
+
+    srand(time(0));
+
+    n = rand() % 10;
+    cout << "Nilai awal n = " << n << endl;
+    while (n < 7)
+    {
+        cout << "Nilai n = " << n << endl;
+        n = rand() % 10;
+        perulanganWhile++;
+    }
+    cout << "Jumlah perulangan = " << perulanganWhile << endl;
+    cout << "Nilai n terakhir = " << n << endl;
+    
+
+
+
+
+}
+    
